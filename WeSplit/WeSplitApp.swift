2d784d841a6cac6ac047269ba247a182195819fc
Phoenix Dai <daizming200149@163.com>
2020-09-26 18:19:23 +0800

    first

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by phoenix Dai on 2020/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
